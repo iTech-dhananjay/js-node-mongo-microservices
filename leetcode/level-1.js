245698838a6a5118abb72cac0fe19193acc1b896
Dhananjay Kumar <officialdhananjay76@gmail.com>
2023-12-27 12:41:23 +0530

    Majority Element - Correct Problem number 169

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -550,9 +550,20 @@ const arr = [1, 2, 3, 2, 3, 4, 2, 6, 2, 3, 5, 2, 7];
 //169. Majority Element
 
 
-// <<<<<<<<<<<<<<<------------------------------------------------- 66. [[ Majority Element ]] -------------------------------------------------------------->>>>>>>>>>>
+// <<<<<<<<<<<<<<<------------------------------------------------- 169. [[ Majority Element ]] -------------------------------------------------------------->>>>>>>>>>>
 /*
 
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
 */
 
 const arr = [2, 2, 1, 1, 1, 2, 2]
