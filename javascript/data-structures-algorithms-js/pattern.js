0cfd02ec48412468b45395b77e2e2d157b3f8745
Dhananjay Kumar <officialdhananjay76@gmail.com>
2023-12-28 17:57:57 +0530

    Add square pattern

--- javascript/data-structures-algorithms-js/pattern.js
+++ javascript/data-structures-algorithms-js/pattern.js
@@ -3,11 +3,11 @@ var n = 5;
 let squarePrint = '';
 
 for (let i = 0; i < n; i++) {
-     for (let j = 0; j < n; j++) {
-          squarePrint += '*';
-     }
-     // newline after each row
-     squarePrint += '\n';
+    for (let j = 0; j < n; j++) {
+        squarePrint += '*';
+    }
+    // newline after each row
+    squarePrint += '\n';
 }
 //First for loop starts with  the 'first row' and 2nd complete the entire columen upto 'n' in first row and so on
 
@@ -20,21 +20,30 @@ console.log(squarePrint);
 *****
 
 */
+let n = 5, string = ""
+for (let i = 1; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        string += "*";
+    }
+    //break
+    string += "\n";
+}
+console.log(string);
 
 // <<<<<<<<<<<<<<<------------------------------------------------ 2. [[ Pyramid Pattern  ]] ---------------------------------------------------------->>>>>>>>>>>
 var n = 5;
 let pyramidPrint = '';
 // External loop
 for (let i = 1; i <= n; i++) {
-     // printing spaces
-     for (let j = 1; j <= n - i; j++) {
-          pyramidPrint += ' ';
-     }
-     // printing star
-     for (let k = 0; k < 2 * i - 1; k++) {
-          pyramidPrint += '*';
-     }
-     pyramidPrint += '\n';
+    // printing spaces
+    for (let j = 1; j <= n - i; j++) {
+        pyramidPrint += ' ';
+    }
+    // printing star
+    for (let k = 0; k < 2 * i - 1; k++) {
+        pyramidPrint += '*';
+    }
+    pyramidPrint += '\n';
 }
 console.log(pyramidPrint);
 /* 
