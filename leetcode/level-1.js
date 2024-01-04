2baebf8924b45d36db79ceefa4d18b751e332935
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-04 11:30:07 +0530

    Add leetcode 28. Find the index of the first occurrence in a string

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -16,6 +16,7 @@
   88.  Merge Sorted Array
   66.  Plus One
   169. Majority Element
+  28. [[ Find the Index of the First Occurrence in a String ]]
 
 
 
@@ -599,3 +600,34 @@ const majorityElement = (arr) => {
 };
 
 console.log(majorityElement(arr));
+
+
+// <<<<<<<<<---------------------- 28. [[ Find the Index of the First Occurrence in a String ]] --------------------------------------->>>>>>>>>>>
+
+//
+// 28. Find the Index of the First Occurrence in a String
+// Easy
+// 5.3K
+// 327
+// Companies
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//
+//
+//
+//     Example 1:
+//
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+//
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+//
+//
+// Constraints:
+//
+//     1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
