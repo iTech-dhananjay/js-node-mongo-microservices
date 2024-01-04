9eb82df3faa561e654e528a6a461266164f63f5b
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-04 12:23:57 +0530

    Add climbing stairs problems

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -603,7 +603,7 @@ const majorityElement = (arr) => {
 console.log(majorityElement(arr));
 
 
-// <<<<<<<<<---------------------- 28. [[ Find the Index of the First Occurrence in a String ]] --------------------------------------->>>>>>>>>>>
+// <<<<<<<<<----------------------------- 28. [[ Find the Index of the First Occurrence in a String ]] ------------------------------------------>>>>>>>>>>>
 
 /*
   - Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
@@ -657,26 +657,18 @@ console.log(hayNeed("sadbutsad", "sad"));
 console.log(hayNeed("leetcode", "leeto"));
 
 
-// 70. Climbing Stairs
-// Easy
-// 20.7K
-// 722
-// Companies
+// <<<<<<<<<----------------------------------------- 28. [[ Climbing Stairs ]] ------------------------------------------------------>>>>>>>>>>>
+
 // You are climbing a staircase. It takes n steps to reach the top.
-//
-//     Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-//
-//
-//
-//     Example 1:
-//
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//Example 1:
 // Input: n = 2
 // Output: 2
 // Explanation: There are two ways to climb to the top.
 // 1. 1 step + 1 step
 // 2. 2 steps
 // Example 2:
-//
+
 // Input: n = 3
 // Output: 3
 // Explanation: There are three ways to climb to the top.
