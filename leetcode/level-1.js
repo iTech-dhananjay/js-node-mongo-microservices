ef5becf4cd12ac1abf88e45fa4c1bfd83f52f3bc
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-05 11:48:56 +0530

    Add comment in length of last word

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -341,8 +341,14 @@ var removeDuplicates = function (nums) {
 */
 
 /**
- * @param {string} s
- * @return {number}
+ * Example 1:
+ Input: s = "Hello World"                          => Output: 5     => Explanation: The last word is "World" with length 5.
+
+ * Example 2:
+ * Input: s = "   fly me   to   the moon  "        => Output: 4     => Explanation: The last word is "moon" with length 4.
+
+ * Example 3:
+ * Input: s = "luffy is still joyboy"              => Output: 6     => Explanation: The last word is "joyboy" with length 6.
  */
 var lengthOfLastWord = function (s) {
     if (s.length === 0) return 0;
