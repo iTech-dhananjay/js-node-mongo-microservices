b1af2e76d7fa91bb821a44cbead93d91b64ef607
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-14 00:06:33 +0530

    Add leetcode-9, Palindrome Number problem with solution

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -927,7 +927,6 @@ console.log(canConstruct(ransomNote, magazine))
 // <<<<<<<<<<------------------------------------------------ 9. [[ Palindrome Number ]] --------------------------------------------------->>>>>>>>>>>
 /*
 *   - Given an integer x, return true if x is palindrome, and false otherwise.
-
     Example 1:
      Input: x = 121  // Output: true
      //Explanation: 121 reads as 121 from left to right and from right to left.
