b08ca4a1ec3ea4cd3e64219ee61f37b0739120b7
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-16 16:19:06 +0530

    Add leetcode-35,Search Insert Position problem

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -21,6 +21,7 @@
   70.  Climbing Stairs [NOT DONE]]
   387. First Unique Character in a String
   383. Ransom Note Problem
+  35. Search Insert Position
 
   :: [[ Medium ]]
   33. Search in Rotated Sorted Array
@@ -961,3 +962,17 @@ function isPalindrome(x) {
 console.log(isPalindrome(121));    // Output: true
 console.log(isPalindrome(-121));   // Output: false
 console.log(isPalindrome(10));     // Output: false
+
+
+// <<<<<<<<<<------------------------------------------------ 35. [[ Search Insert Position ]] --------------------------------------------------->>>>>>>>>>>
+
+/*
+*
+* =>   Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+       You must write an algorithm with O(log n) runtime complexity.
+       Example 1:  Input: nums = [1,3,5,6], target = 5             // Output: 2
+       Example 2:  Input: nums = [1,3,5,6], target = 2             // Output: 1
+       Example 3:  Input: nums = [1,3,5,6], target = 7             // Output: 4
+
+* */
+
