cb4e72e6a5fd479e1aefac5fe8e2c822f8d9c460
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-21 03:21:49 +0530

    Add leetcode-27 ,Remove Element with solution

--- leetcode/level-1.js
+++ leetcode/level-1.js
@@ -1128,4 +1128,73 @@ while (result !== null) {
 }
 console.log(output); // Output: [1, 2, 3]
 
+/*
+* Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+*
+*
+*
+*
+*
+* */
+var removeElement = function (nums, val) {
+    let k = 0; // Variable to track the count of elements not equal to val
+
+    // Iterate through the array
+    for (let i = 0; i < nums.length; i++) {
+        // If the current element is not equal to val, update nums array
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+// Test cases
+let nums1 = [3, 2, 2, 3];
+let val1 = 3;
+console.log(removeElement(nums1, val1)); // Output: 2, nums1 = [2, 2, _, _]
+
+let nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
+let val2 = 2;
+console.log(removeElement(nums2, val2)); // Output: 5, nums2 = [0, 1, 4, 0, 3, _, _, _]
