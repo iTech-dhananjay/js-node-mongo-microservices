14a9f4751bafc04982ab3647d6d79554c9e7d91c
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-23 14:45:42 +0530

    Fix unique combinations from list

--- index2.js
+++ index2.js
@@ -1,14 +1,33 @@
-const arrObj = [
+const data = [
     {name: 'abc', slug: 'abc'},
-    {name: 'pqr', slug: 'xyz'},
+    {name: 'xyz', slug: 'abc'},
     {name: 'abc', slug: 'abc'},
-    {name: 'abc', slug: 'xyz'}
+    {name: 'xyz', slug: 'pqr'}
 ];
 
-function isUniqueSlug(value, index, self) {
-    return self.findIndex(obj => obj.slug === value.slug) === index;
+const uniqueCombine = (items) => {
+    const uniqueCombinations = [];
+    for (let i = 0; i < items.length; i++) {
+        const currentItem = items[i];
+        let isUnique = true;
+
+        // Check if the current combination is unique
+        for (let j = 0; j < uniqueCombinations.length; j++) {
+            const existingItem = uniqueCombinations[j];
+            if (currentItem.name === existingItem.name && currentItem.slug !== existingItem.slug) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        // If unique, add to the result array
+        if (isUnique) {
+            uniqueCombinations.push(currentItem);
+        }
+    }
+
+    return uniqueCombinations;
 }
 
-const uniqueArrObj = arrObj.filter(isUniqueSlug);
-
-console.log(uniqueArrObj);
+// Output: [{name: 'abc', slug: 'abc'}, {name: 'xyz', slug: 'pqr'}]
+console.log(uniqueCombine(data));
