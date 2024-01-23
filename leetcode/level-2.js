ac712e82da617a7c0980caeca270f828d3ce8e24
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-23 18:02:05 +0530

    Merge Objects with Null

--- leetcode/level-2.js
+++ leetcode/level-2.js
@@ -38,6 +38,44 @@ Explanation: 7/-3 = -2.33333.. which is truncated to -2.
 *
 * */
 
-var divide = function (dividend, divisor) {
-
+const user1 = {
+    name: "Rajan",
+    age: null,
+    address: {
+        pincode: 1234444,
+        city: null,
+    },
+    test: null,
 };
+
+const user2 = {
+    name: "Piyush",
+    age: null,
+    address: {
+        pincode: 843434,
+        city: "GGN",
+        state: "haryana",
+    },
+    mobile: 1234567890,
+};
+
+function mergeObjects(obj1, obj2) {
+    const result = {...obj2};
+
+    for (const key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (obj1[key] !== null && typeof obj1[key] === 'object' && result[key]) {
+                // Recursively merge nested objects
+                result[key] = mergeObjects(obj1[key], result[key]);
+            } else if (obj1[key] !== null || result[key] === undefined) {
+                result[key] = obj1[key];
+            }
+        }
+    }
+
+    return result;
+}
+
+
+const mergedUser = mergeObjects(user1, user2);
+console.log(mergedUser);
