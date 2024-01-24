d344543fbf6826c85f5aba49f6532fb566539e47
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-24 15:46:09 +0530

    Merge Objects with Null handle with condition

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-let number = 8;
+const user1 = {
+    name: "Rajan",
+    age: null,
+    address: {
+        pincode: 1234444,
+        city: null,
+    },
+    test: null,
+};
 
-const mysqrt = (x) => {
-    for (let i = 0; i * i <= x; i++) {
-        if (i * i === x) {
-            console.log(i);
-            return i;
+const user2 = {
+    name: "Piyush",
+    age: null,
+    address: {
+        pincode: 843434,
+        city: "GGN",
+        state: "haryana",
+    },
+    mobile: 1234567890,
+};
+
+
+function mergeObjects(obj1, obj2) {
+    const result = {...obj2};
+
+    for (const key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (obj1[key] !== null && typeof obj1[key] === 'object' && result[key]) {
+                // Recursively merge nested objects
+                result[key] = mergeObjects(obj1[key], result[key]);
+            } else if (obj1[key] !== null || result[key] === undefined) {
+                result[key] = obj1[key];
+            }
         }
     }
-    return x;
+
+    return result;
 }
 
-console.log(mysqrt(number))
+
+console.log(mergeObjects(user1, user2))
