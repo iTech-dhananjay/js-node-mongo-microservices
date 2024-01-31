ae2fb997565e7e3e1198afb9161cf7eb86222462
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-31 13:24:24 +0530

    Add explanation to call,apply && bind

--- javascript/concept-overview.js
+++ javascript/concept-overview.js
@@ -949,3 +949,34 @@ person.greet();  //  {name: "Jack", age: 25, greet: ƒ}  || Jack   => In the abo
 
 // <<<<<<<<<<<<<----------------------------------------- 16. [[ call() apply() && bind()  ]] ----------------------------------------------------->>>>>>>>>>>
 
+/*
+     =>  Working with JavaScript “this” keyword can be tricky. Not knowing the background rules may end up with the famous “it works, but I don’t know why” or worse: “it doesn’t work and
+         I don’t know why”. It’s good to know the theory before putting things into practice. Call(), Apply() and Bind() methods can come in handy when setting the “this” value.
+
+     =>  Basic rules worth remembering :
+           1. -> “this” always refers to an object.
+           2. -> “this” refers to an object which calls the function it contains.
+           3. ->  In the global context “this” refers to either window object or is undefined if the ‘strict mode’ is used.
+
+* */
+
+var car = {
+    registrationNumber: "GA12345",
+    brand: "Toyota",
+
+    displayDetails: function () {
+        console.log(this.registrationNumber + " " + this.brand);
+    }
+}
+
+/*
+    - The above will work perfectly fine as long as we use it this way: car.displayDetails(); // GA12345 Toyota
+ VVI- But what if we want to borrow a method?
+           var myCarDetails =  car.displayDetails;
+           myCarDetails();
+    - Well, this won’t work as the “this” will be now assigned to the global context which doesn’t have neither the registrationNumber nor the brand property.
+* */
+
+//The bind() Method - This means bind() can be used by every single function.
+var myCarDetails = car.displayDetails.bind(car);
+myCarDetails(); // GA12345 Toyota
