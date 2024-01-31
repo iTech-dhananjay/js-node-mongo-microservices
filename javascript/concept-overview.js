ce84b1c8a6a44274680515709347565543eaae2f
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-01-31 14:11:17 +0530

    Add explanation to call,apply && bind v-1

--- javascript/concept-overview.js
+++ javascript/concept-overview.js
@@ -975,8 +975,50 @@ var car = {
            var myCarDetails =  car.displayDetails;
            myCarDetails();
     - Well, this won’t work as the “this” will be now assigned to the global context which doesn’t have neither the registrationNumber nor the brand property.
+
+    //The bind() Method - This means bind() can be used by every single function.
+     var myCarDetails = car.displayDetails.bind(car);
+     myCarDetails(); // GA12345 Toyota
+
+    - The bind() method creates a new function where “this” refers to the parameter in the parenthesis in the above case “car”.
+      This way the bind() method enables calling a function with a specified “this” value.
+    - What if we would like to pass a parameter to the displayDetails function? We can use the bind method again.
+      The following argument of the bind() method will provide an argument to the function bind() is called on.
 * */
 
-//The bind() Method - This means bind() can be used by every single function.
-var myCarDetails = car.displayDetails.bind(car);
-myCarDetails(); // GA12345 Toyota
+var car = {
+    registrationNumber: "GA12345",
+    brand: "Toyota",
+
+    displayDetails: function (ownerName) {
+        console.log(ownerName + ", this is your car: " + this.registrationNumber + " " + this.brand);
+    }
+}
+//Example of passing arguments with bind():
+var myCarDetails = car.displayDetails.bind(car, "Vivian"); // Vivian, this is your car: GA12345 Toyota
+
+
+// 2nd Part  [[ call() and apply() methods ]]
+
+/*
+   => Similar but slightly different usage provide the call() and apply() methods which also belong to the Function.prototype property.
+      This time there is a car object without the displayDetails function, which is located in the global context.
+   => Note that when using the apply() function the parameter must be placed in an array. Call() accepts both an array of parameters and a parameter itself.
+      Both are great tools for borrowing functions in JavaScript.
+   => bind(), call() and apply() functions can make your life easier when you need to set the value of ‘this’. Hope the post was helpful.
+* */
+
+var car = {
+    registrationNumber: "GA12345",
+    brand: "Toyota"
+}
+
+function displayDetails(ownerName) {
+    console.log(ownerName + ", this is your car: " + this.registrationNumber + " " + this.brand);
+}
+
+//We can use the apply() function:
+displayDetails.apply(car, ["Vivian"]); // Vivian, this is your car: GA12345 Toyota
+
+//Or
+displayDetails.call(car, "Vivian"); // Vivian, this is your car: GA12345 Toyota
