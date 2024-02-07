f312526de407b852ed6883529ab27db9518ae221
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-02-07 15:08:54 +0530

    Add Concurrency and Parallelism,Security Best Practices,Performance Optimization,Streams

---
+++ Quick-Comp/node-advance-1.js
@@ -0,0 +1,13 @@
+// <<<<<<<<<<<<<<<-----------------------------------------------  [[ Node.js Advanced Concepts ]] ------------------------------------------------>>>>>>>>>>>
+/*
+  1.  Event Loop
+  2.  Streams
+  3.  Promises and Async/Await
+  4.  Concurrency and Parallelism
+  5.  Debugging and Profiling
+  6.  Microservices Architecture
+  7.  Security Best Practices
+  8.  Performance Optimization
+  9.  Scalability:
+  10. Dependency Management and Package Security
+*/
