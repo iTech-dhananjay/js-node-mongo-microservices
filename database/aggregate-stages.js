576d780232b43f2937ee012e087a50597c6e414e
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-02-12 12:16:30 +0530

    Add mongodb aggregation stages with logic with match

--- database/aggregate-stages.js
+++ database/aggregate-stages.js
@@ -22,6 +22,7 @@ The MongoDB aggregation pipeline consists of several stages that allow you to pr
 
 // Q. =>  SELECT * FROM `posts` RIGHT JOIN users ON users.id=posts.user_id ORDER BY posts.id DESC GROUP BY posts.user_id
 
+
 // Create users
 db.users.insertMany([
     {_id: 1, name: "User1"},
@@ -32,8 +33,17 @@ db.users.insertMany([
 // Create posts
 db.posts.insertMany([
     {_id: 101, userId: 1, title: "Post1"},
-    {_id: 102, userId: 2, title: "Post2"},
+    {_id: 102, userId: 1, title: "Post2"},
+    {_id: 103, userId: 2, title: "Post3"},
+    {_id: 104, userId: 2, title: "Post4"},
 ]);
+
+
+db.posts.updateMany({}, {$set: {isPublished: false}});
+
+// Set isPublished to true for some posts
+db.posts.updateMany({_id: {$in: [101, 103]}}, {$set: {isPublished: true}});
+
 
 db.users.aggregate([
     {
@@ -48,9 +58,13 @@ db.users.aggregate([
         $unwind: "$result"
     },
     {
+        $match: {
+            "result.isPublished": true
+        }
+    },
+    {
         $sort: {"result._id": -1}
     },
-
     {
         $group: {
             _id: "$_id",
@@ -59,31 +73,21 @@ db.users.aggregate([
         }
     },
     {
-        $skip: 1, // Skip the first document
+        $skip: 0, // Skip the first document
     },
     {
-        $limit: 2, // Limit the result to 2 documents
+        $limit: 4, // Limit the result to 2 documents
     },
 
 ]).pretty()
 
-
 /*
  Output : -
 
- { "acknowledged" : true, "insertedIds" : [ 1, 2, 3 ] }
-{ "acknowledged" : true, "insertedIds" : [ 101, 102 ] }
-{
-	"_id" : 2,
-	"name" : "User2",
-	"posts" : [
-		{
-			"_id" : 102,
-			"userId" : 2,
-			"title" : "Post2"
-		}
-	]
-}
+{ "acknowledged" : true, "insertedIds" : [ 1, 2, 3 ] }
+{ "acknowledged" : true, "insertedIds" : [ 101, 102, 103, 104 ] }
+{ "acknowledged" : true, "matchedCount" : 4, "modifiedCount" : 4 }
+{ "acknowledged" : true, "matchedCount" : 2, "modifiedCount" : 2 }
 {
 	"_id" : 1,
 	"name" : "User1",
@@ -91,7 +95,20 @@ db.users.aggregate([
 		{
 			"_id" : 101,
 			"userId" : 1,
-			"title" : "Post1"
+			"title" : "Post1",
+			"isPublished" : true
+		}
+	]
+}
+{
+	"_id" : 2,
+	"name" : "User2",
+	"posts" : [
+		{
+			"_id" : 103,
+			"userId" : 2,
+			"title" : "Post3",
+			"isPublished" : true
 		}
 	]
 }
@@ -99,108 +116,5 @@ db.users.aggregate([
 * */
 
 
-// Create users collection and insert documents
-db.users.insertMany([
-    {
-        name: "Dhananjay Kumar",
-        age: 22,
-        gender: "male",
-        maritalStatus: "single",
-        shoppingList: ["electronic-gadgets", "books", "laptop"]
-    },
-    {
-        name: "Pooja",
-        age: 20,
-        gender: "female",
-        maritalStatus: "single",
-        shoppingList: ["dress", "shoes", "jewelry"]
-    },
-    {
-        name: "Alice",
-        age: 24,
-        gender: "female",
-        maritalStatus: "single",
-        shoppingList: ["gadgets", "clothes"]
-    }
-]);
-
-// Create marriage_shopping_events collection and insert documents
-db.marriage_shopping_events.insertMany([
-    {
-        eventName: "Wedding",
-        items: ["wedding dress", "suit", "rings", "flowers", "cake"]
-    },
-    {
-        eventName: "Anniversary",
-        items: ["gift", "dinner reservations", "flowers"]
-    },
-    {
-        eventName: "Engagement",
-        items: ["engagement ring", "proposal location", "flowers"]
-    }
-]);
-// Aggregation on users collection
-const userAggregation = db.users.aggregate([
-    {
-        $match: {
-            gender: "female" // Match only female users
-        }
-    },
-    {
-        $project: {
-            name: 1,
-            age: 1,
-            maritalStatus: 1,
-            shoppingList: 1
-        }
-    },
-    {
-        $group: {
-            _id: "$maritalStatus",
-            users: {
-                $push: {
-                    name: "$name",
-                    age: "$age",
-                    shoppingList: "$shoppingList"
-                }
-            }
-        }
-    },
-    {
-        $sort: {
-            _id: 1 // Sort by marital status
-        }
-    }
-]);
-
-// Aggregation on marriage_shopping_events collection
-const eventAggregation = db.marriage_shopping_events.aggregate([
-    {
-        $project: {
-            eventName: 1,
-            items: 1
-        }
-    },
-    {
-        $unwind: "$items"
-    },
-    {
-        $group: {
-            _id: "$eventName",
-            shoppingList: {$push: "$items"}
-        }
-    }
-]);
-
-// Print the results
-print("User Aggregation:");
-while (userAggregation.hasNext()) {
-    printjson(userAggregation.next());
-}
-
-print("\nMarriage Shopping Event Aggregation:");
-while (eventAggregation.hasNext()) {
-    printjson(eventAggregation.next());
-}
 
 
