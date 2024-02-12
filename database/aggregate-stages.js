d53cc265e39d3dbea4c03db9a3310d2e76b8f47e
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-02-12 12:39:52 +0530

    Add concepts relatd to right and left join

--- database/aggregate-stages.js
+++ database/aggregate-stages.js
@@ -21,6 +21,13 @@ The MongoDB aggregation pipeline consists of several stages that allow you to pr
 
 
 // Q. =>  SELECT * FROM `posts` RIGHT JOIN users ON users.id=posts.user_id ORDER BY posts.id DESC GROUP BY posts.user_id
+/*Different Types of SQL JOINs
+Here are the different types of the JOINs in SQL:
+
+(INNER) JOIN: Returns records that have matching values in both tables
+LEFT (OUTER) JOIN: Returns all records from the left table, and the matched records from the right table
+RIGHT (OUTER) JOIN: Returns all records from the right table, and the matched records from the left table
+FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table*/
 
 
 // Create users
