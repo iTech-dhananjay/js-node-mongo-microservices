8a071eddf5877e21e93d9b7bdb671a73bd88131f
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-02-19 12:24:05 +0530

    Add file type conversion in KB and MB

--- level-1-profile/PaginationLogic.js
+++ level-1-profile/PaginationLogic.js
@@ -1,3 +1,21 @@
+let inputBytes = 21369;
+
+//Convert Size to KB/MB
+function bytesToSize(bytes) {
+    const kilobytes = bytes / 1024;
+    const megabytes = kilobytes / 1024;
+
+    if (megabytes >= 1) {
+        return megabytes.toFixed(2) + " MB";
+    } else {
+        return kilobytes.toFixed(2) + " KB";
+    }
+}
+
+const size = bytesToSize(inputBytes);
+console.log(size);
+
+
 /*
      JSON.stringify() :
          - The JSON.stringify() static method converts a JavaScript value to a JSON string.JavaScript Object Notation
