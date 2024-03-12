93439827e80b66e8116e5a7fe5b15d15e7513cb6
Dhananjay Kumar <officialdhananjay76@gmail.com>
2024-03-12 14:46:26 +0530

    Add description in NGINX

---
+++ Quick-Comp/cloud-node.js
@@ -0,0 +1,20 @@
+//Round robin algorithm
+
+/*
+    - NGINX is a powerful web server and uses a non-threaded, event-driven architecture.
+    - It can also do other important things, such as load balancing, and HTTP caching, or be used as a reverse proxy.
+    - A reverse proxy utilizes multiple servers but serves a single client. NGINX determines the appropriate server for sending requests and fetching responses, thereby facilitating load balancing.
+         - Can Handle 10,000 Concurrent Requests
+         - Cache HTTP Requests
+         - Act as Reverse Proxy
+         - Act as Load Balancer
+         - Act as an API Gateway
+         - Serve and Cache Static files like images, videos, etc.
+         - Handle SSL Certificates
+*/
+
+
+
+
+
+
